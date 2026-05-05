29ed5b6a9c5358fc86840bfe2d3e934feb106725
Khushi Tirkey <khushitirkey423@gmail.com>
2026-05-05 21:27:28 +0530

    Add: sum-of-two-integers - submission-1

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        max_len=0x7fffffff
+        mask=0xffffffff
+        while b!=0:
+            carry = (a & b)<<1
+            a=( a^b)&mask
+            b=carry
+        return a if a < max_len else ~(a ^ mask)
+        
+        
