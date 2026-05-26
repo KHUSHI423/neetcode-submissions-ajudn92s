3e184bd16fa874d92704265be814ad01dfac15f6
Khushi Tirkey <khushitirkey423@gmail.com>
2026-05-26 22:52:45 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        #space optimization
+        if n<=2:
+            return n
+        prev=0
+        curr=1
+        while n:
+            prev,curr=curr,curr+prev
+            n-=1
+        return curr
+        
