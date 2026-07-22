2144d912008ec5e3e9fb35b55ad58fb3ae8c2650
Khushi Tirkey <khushitirkey423@gmail.com>
2026-07-22 18:25:26 +0530

    Add: n-th-tribonacci-number - submission-1

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        dp =[0]*(n+1)
+        def depth(n):
+            if n<=0:
+                return 0
+            if n == 1:
+                return 1
+            if dp[n]!=0:
+                return dp[n]
+            dp[n] = depth(n-1) + depth(n-2) + depth(n-3)
+            return dp[n]
+        return depth(n)
